dd914125ccb76ff160c40ad601b157f9938808e1
Marco19987 <71755834+Marco19987@users.noreply.github.com>
2023-07-18 12:07:56 +0200

    relative path acados env vars

--- acados_nmpc/env_vars_acados.m
+++ acados_nmpc/env_vars_acados.m
@@ -1,6 +1,7 @@
 
 
-model_path = 'C:\Users\saraf\acados\examples\acados_matlab_octave';
+model_path = fullfile(pwd,'..\..\acados\examples\acados_matlab_octave');
+% 'C:\Users\Marco\acados\examples\acados_matlab_octave';
 addpath(model_path)
 
 acados_env_variables_windows;
