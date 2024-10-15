f438e1ce689c66597411e54de2e0748a88e109ff
Sara Federico <sara.federico99@hotmail.it>
2024-10-15 11:33:51 +0200

    gitignore fixed

--- acados_nmpc/env_vars_acados.m
+++
@@ -1,7 +0,0 @@
-
-
-model_path = fullfile(pwd,'..\..\acados\examples\acados_matlab_octave');
-% 'C:\Users\Marco\acados\examples\acados_matlab_octave';
-addpath(model_path)
-
-acados_env_variables_windows;
--- acados_nmpc/env_vars_acados_linux.m
+++
@@ -1,4 +0,0 @@
-% model_path = "/home/workstation/acados/examples/acados_matlab_octave/getting_started/";
-% addpath(model_path)
-
-system('source env.sh');
