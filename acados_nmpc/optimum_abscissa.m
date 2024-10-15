9f1e2cdb5ad470df77310a4436e69717142b92d2
Sara Federico <sara.federico99@hotmail.it>
2024-10-15 12:15:23 +0200

    gitignore fixed

--- acados_nmpc/optimum_abscissa.m
+++
@@ -1,3 +0,0 @@
-function f = optimum_abscissa(x,c)
-    f = norm([c.S_p_x c.S_p_y] - full(c.fc_spline(x)))^2;
-end
--- acados_nmpc/test_optimum_abscissa.m
+++
@@ -1,6 +0,0 @@
-
-function [x,fval] = test_optimum_abscissa(s0_spline,c)
-% options = optimoptions('fmincon','Display','off','UseParallel',false);
-options = optimoptions('fmincon','Algorithm','active-set','Display','off','StepTolerance',1e-4,'OptimalityTolerance',1e-4);
-[x, fval] = fmincon(@(x) optimum_abscissa(x,c),s0_spline,0,0,0,0,[],[],[],options);
-end
